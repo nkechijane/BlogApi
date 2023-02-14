a04c5d37277984dc01e26975d757f982f824bd1c
Okeke Nkechi Jane <janecutie89@gmail.com>
2023-02-14 15:52:40 +0100

    Add GetAll on Author Controller

--- BlogAPI/Controllers/AuthorController.cs
+++ BlogAPI/Controllers/AuthorController.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace BlogAPI.Controllers;
 
 [Controller]
 [Route("[controller]")]
 public class AuthorController : ControllerBase
 {
-    
+    [HttpGet("getall")]
+    public async Task<IActionResult> GetAll()
+    {
+        var authors = new
+        {
+            FirstName = "Tina",
+            MiddleName = "Doe",
+            LastName = "Effiong"
+        };
+        return Ok(authors);
+    }
 }
